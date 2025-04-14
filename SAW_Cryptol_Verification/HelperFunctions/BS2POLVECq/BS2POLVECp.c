ba58b3feb96ac317b73c169dd07a16aa856679c8
sydney <sydney@kali>
2025-04-14 15:21:36 -0400

    helper updates

--- SAW_Cryptol_Verification/HelperFunctions/BS2POLVECq/BS2POLVECp.c
+++ SAW_Cryptol_Verification/HelperFunctions/BS2POLVECq/BS2POLVECp.c
@@ -1,13 +1,21 @@
+#include "../pack_unpack.h"
 
-#include <stdint.h>
+void BS2POLVECp(const unsigned char *bytes, uint16_t data[SABER_K][SABER_N]){
+	
+	uint32_t i,j;
+	uint32_t offset_data=0,offset_byte=0,offset_byte1=0;	
+	
+	offset_byte=0;
+	for(i=0;i<SABER_K;i++){
+		offset_byte1=i*(SABER_N*10)/8;
+		for(j=0;j<SABER_N/4;j++){
+			offset_byte=offset_byte1+5*j;
+			offset_data=4*j;
+			data[i][offset_data + 0]= ( bytes[ offset_byte + 0 ] & (0xff)) |  ((bytes[ offset_byte + 1 ] & 0x03)<<8);
+			data[i][offset_data + 1]= ( (bytes[ offset_byte + 1 ]>>2) & (0x3f)) |  ((bytes[ offset_byte + 2 ] & 0x0f)<<6);		
+			data[i][offset_data + 2]= ( (bytes[ offset_byte + 2 ]>>4) & (0x0f)) |  ((bytes[ offset_byte + 3 ] & 0x3f)<<4);
+			data[i][offset_data + 3]= ( (bytes[ offset_byte + 3 ]>>6) & (0x03)) |  ((bytes[ offset_byte + 4 ] & 0xff)<<2);		
 
-#define L 3
-#define N 256
-
-void BS2POLVECp(const uint8_t *bytes, uint16_t vec[L][N]) {
-    for (int i = 0; i < L; i++) {
-        for (int j = 0; j < N; j++) {
-            vec[i][j] = (uint16_t)(bytes[i * N + j]);
-        }
-    }
+		}
+	}
 }
