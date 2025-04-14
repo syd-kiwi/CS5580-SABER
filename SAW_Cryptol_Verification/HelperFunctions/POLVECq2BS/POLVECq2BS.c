e7604291c5b8df424c096982302399a98235e966
syd-kiwi <sydneyjohns@vt.edu>
2025-04-14 15:10:01 -0400

    Update POLVECq2BS.c

--- SAW_Cryptol_Verification/HelperFunctions/POLVECq2BS/POLVECq2BS.c
+++ SAW_Cryptol_Verification/HelperFunctions/POLVECq2BS/POLVECq2BS.c
@@ -1,13 +1,25 @@
+#include "../pack_unpack.h"
 
-#include <stdint.h>
+void POLVECp2BS(uint8_t *bytes, uint16_t data[SABER_K][SABER_N]){
+	
+	uint32_t i,j;
+	uint32_t offset_data=0,offset_byte=0,offset_byte1=0;	
+	
+	offset_byte=0;
+	for(i=0;i<SABER_K;i++){
+		offset_byte1=i*(SABER_N*10)/8;
+		for(j=0;j<SABER_N/4;j++){
+			offset_byte=offset_byte1+5*j;
+			offset_data=4*j;
+			bytes[offset_byte + 0]= ( data[i][ offset_data + 0 ] & (0xff));
 
-#define L 3
-#define N 256
+			bytes[offset_byte + 1]= ( (data[i][ offset_data + 0 ] >>8) & 0x03 ) | ((data[i][ offset_data + 1 ] & 0x3f) << 2);
 
-void POLVECq2BS(uint8_t *bytes, const uint16_t vec[L][N]) {
-    for (int i = 0; i < L; i++) {
-        for (int j = 0; j < N; j++) {
-            bytes[i * N + j] = (uint8_t)(vec[i][j] & 0xFF);
-        }
-    }
+			bytes[offset_byte + 2]= ( (data[i][ offset_data + 1 ] >>6) & 0x0f ) | ( (data[i][ offset_data + 2 ] &0x0f) << 4);
+
+			bytes[offset_byte + 3]= ( (data[i][ offset_data + 2 ] >>4) & 0x3f ) | ((data[i][ offset_data + 3 ] & 0x03) << 6);
+
+			bytes[offset_byte + 4]= ( (data[i][ offset_data + 3 ] >>2) & 0xff );
+		}
+	}
 }
